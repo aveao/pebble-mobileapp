e9652f3748e5c1cd8e1782cc398f6e24390077a9
ave <git@ave.zone>
2026-04-16 08:27:44 +0200

    fix for beeper type for ring

--- composeApp/src/commonMain/kotlin/coredevices/coreapp/backup/BackupManifest.kt
+++ composeApp/src/commonMain/kotlin/coredevices/coreapp/backup/BackupManifest.kt
@@ -1,5 +1,6 @@
 package coredevices.coreapp.backup
 
+import coredevices.ring.agent.builtin_servlets.messaging.ApprovedBeeperContact
 import coredevices.ring.database.MusicControlMode
 import coredevices.ring.database.SecondaryMode
 import coredevices.util.CoreConfig
@@ -22,7 +23,7 @@ data class RingPreferencesExport(
     val musicControlMode: Int = MusicControlMode.DoubleClick.id,
     val lastSyncIndex: Int? = null,
     val debugDetailsEnabled: Boolean = false,
-    val approvedBeeperContacts: List<String> = emptyList(),
+    val approvedBeeperContacts: List<ApprovedBeeperContact> = emptyList(),
     val secondaryMode: Int = SecondaryMode.Search.id,
     val reminderProvider: Int = 1,
     val noteProvider: Int = 1,
