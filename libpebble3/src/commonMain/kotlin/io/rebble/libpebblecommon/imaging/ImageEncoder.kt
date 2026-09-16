coredevices/mobileapp

imaging: match pixels against the colors the panel really shows
The encoder picks a palette entry for each pixel by nearest neighbor in RGB,
and it placed those entries at the nominal 0/85/170/255 the two GColor8 bits
imply. This is not representative of the actual screen behavior,
especially around saturation.

This adds 64 colors measured off a Pebble Time 2 under sunlight, color
corrected with a SpyderChekr color grid, and adjusts the
nearest neighbor search and Floyd-Steinberg residual to compute against
them.

The measured colors are normalized so the panel's own black and white land on
0 and 255, as the actual contrast is significantly (~5x) lower under sunlight.

The wire format is untouched: palette bytes are still GColor8 codes.

Co-Authored-By: Claude Opus 5 (1M context) <noreply@anthropic.com>
Signed-off-by: ave <git@ave.zone>

libpebble3/src/commonMain/kotlin/io/rebble/libpebblecommon/imaging/ImageEncoder.kt
@@ -4,13 +4,15 @@ package io.rebble.libpebblecommon.imaging
  * Encodes an ARGB image as the watch's 4-bpp palettized [EncodedImage]: choose a 16-colour palette
  * by median cut over the watch's 64-colour (GColor8) space, Floyd–Steinberg dither to that palette,
  * and pack two 4-bit indices per byte (even x = high nibble, matching the firmware).
+ *
+ * Pixels are matched against what the panel actually shows (see ScreenColors.kt), not the nominal
+ * 0/85/170/255 the GColor8 bits suggest. The bytes on the wire are still GColor8 codes.
  */
 object ImageEncoder {
     private const val MAX_COLORS = 16
 
-    // 8-bit channel (0..255) -> 2-bit GColor8 channel (0..3), rounded to nearest of 0/85/170/255.
+    // 8-bit channel (0..255) -> 2-bit GColor8 channel (0..3).
     private fun quant2(v: Int): Int = (v.coerceIn(0, 255) * 3 + 127) / 255
-    private fun expand2(c: Int): Int = c * 85
     private fun gcolor8(r: Int, g: Int, b: Int): Int = (0x3 shl 6) or (r shl 4) or (g shl 2) or b
 
     private class Color(val r: Int, val g: Int, val b: Int, val count: Int)
@@ -18,9 +20,9 @@ object ImageEncoder {
     /** Encodes an ARGB8888 pixel array (row-major, [width] * [height]). */
     fun encode(argb: IntArray, width: Int, height: Int): EncodedImage {
         val palette = medianCutPalette(argb)
-        val palR = IntArray(palette.size) { expand2((palette[it] shr 4) and 0x3) }
-        val palG = IntArray(palette.size) { expand2((palette[it] shr 2) and 0x3) }
-        val palB = IntArray(palette.size) { expand2(palette[it] and 0x3) }
+        val palR = IntArray(palette.size) { SCREEN_R[palette[it] and 0x3F] }
+        val palG = IntArray(palette.size) { SCREEN_G[palette[it] and 0x3F] }
+        val palB = IntArray(palette.size) { SCREEN_B[palette[it] and 0x3F] }
 
         val stride = (width + 1) / 2
         val pixels = UByteArray(stride * height)

aveao/pebble-mobileapp

libpebble3/src/commonMain/kotlin/io/rebble/libpebblecommon/imaging/ImageEncoder.kt
@@ -7,6 +7,9 @@ package io.rebble.libpebblecommon.imaging
  *
  * Transparency is binary, and costs a palette slot when present. The watch only skips these
  * pixels when it draws with `GCompOpSet`; under the default `GCompOpAssign` they come out black.
+ *
+ * Pixels are matched against what the panel actually shows (see ScreenColors.kt), not the nominal
+ * 0/85/170/255 the GColor8 bits suggest. The bytes on the wire are still GColor8 codes.
  */
 object ImageEncoder {
     private const val MAX_COLORS = 16
@@ -19,9 +22,8 @@ object ImageEncoder {
 
     private fun alphaOf(argb: Int): Int = (argb ushr 24) and 0xFF
 
-    // 8-bit channel (0..255) -> 2-bit GColor8 channel (0..3), rounded to nearest of 0/85/170/255.
+    // 8-bit channel (0..255) -> 2-bit GColor8 channel (0..3).
     private fun quant2(v: Int): Int = (v.coerceIn(0, 255) * 3 + 127) / 255
-    private fun expand2(c: Int): Int = c * 85
     private fun gcolor8(r: Int, g: Int, b: Int): Int = (0x3 shl 6) or (r shl 4) or (g shl 2) or b
 
     private class Color(val r: Int, val g: Int, val b: Int, val count: Int)
@@ -30,9 +32,9 @@ object ImageEncoder {
     fun encode(argb: IntArray, width: Int, height: Int): EncodedImage {
         val hasTransparency = argb.any { alphaOf(it) < OPAQUE_ALPHA }
         val colors = medianCutPalette(argb, if (hasTransparency) MAX_COLORS - 1 else MAX_COLORS)
-        val palR = IntArray(colors.size) { expand2((colors[it] shr 4) and 0x3) }
-        val palG = IntArray(colors.size) { expand2((colors[it] shr 2) and 0x3) }
-        val palB = IntArray(colors.size) { expand2(colors[it] and 0x3) }
+        val palR = IntArray(colors.size) { SCREEN_R[colors[it] and 0x3F] }
+        val palG = IntArray(colors.size) { SCREEN_G[colors[it] and 0x3F] }
+        val palB = IntArray(colors.size) { SCREEN_B[colors[it] and 0x3F] }
         // Past the colours, so `nearest` can never land an opaque black pixel on it.
         val transparentIdx = colors.size
         val palette = if (hasTransparency) colors + TRANSPARENT else colors